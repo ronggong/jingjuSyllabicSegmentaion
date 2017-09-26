e936428cc2a56ee25fa660054823604d4ee187ee
ronggong <ronggong1010@gmail.com>
2017-09-26 21:05:20 +0200

    manually add cuda path

--- training_scripts/hpcDLScripts/keras_cnn_syllableSeg_conv_dense_jordi.sh
+++ training_scripts/hpcDLScripts/keras_cnn_syllableSeg_conv_dense_jordi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # change python version
-module load cuda/8.0
+#module load cuda/8.0
 #module load theano/0.8.2
 
 # two variables you need to set
@@ -12,6 +12,7 @@ device=gpu0  # the device to be used. set it to "cpu" if you don't have GPUs
 export PATH=/homedtic/rgong/anaconda2/bin:$PATH
 export THEANO_FLAGS=mode=FAST_RUN,device=$device,floatX=float32,lib.cnmem=0.95
 export PATH=/usr/local/cuda/bin/:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/soft/cuda/cudnn/cuda/lib64:$LD_LIBRARY_PATH
 export CPATH=/soft/cuda/cudnn/cuda/include:$CPATH
 export LIBRARY_PATH=/soft/cuda/cudnn/cuda/lib64:$LD_LIBRARY_PATH
