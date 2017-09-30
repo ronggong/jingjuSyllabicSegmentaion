3a78e27d47d6878331254761d582e2e46922a176
ronggong <ronggong1010@gmail.com>
2017-09-30 15:57:18 +0200

    new partition training test artist

--- src/trainTestSeparation.py
+++ src/trainTestSeparation.py
@@ -97,7 +97,11 @@ def getBoundaryNumber(recordings, dataset_path):
         listOnset += [[recording_name, numOnset]]
     return listOnset
 
-def getTestTrainRecordings():
+def getTestTrainRecordingsSex():
+    """
+    partition by male and female
+    :return:
+    """
     lineOnsetQmMale = getBoundaryNumber(queenMaryMale_Recordings, queenMarydataset_path)
     lineOnsetQmFem = getBoundaryNumber(queenMaryFem_Recordings, queenMarydataset_path)
     lineOnsetLon = getBoundaryNumber(london_Recordings, londonRecording_path)
@@ -147,6 +151,135 @@ def getTestTrainRecordings():
     print numOnsetSum
     print numOnsetSumTest
 
+def recordingsNumTestTrainHelper(lineOnset, idx_test):
+    recordingsTest = [lineOnset[ii][0] for ii in idx_test]
+    recordingsTrain = [lineOnset[ii][0] for ii in range(len(lineOnset)) if
+                              ii not in idx_test]
+    numTest= [lineOnset[ii][1] for ii in idx_test]
+    numTrain = [lineOnset[ii][1] for ii in range(len(lineOnset)) if ii not in idx_test]
+    return recordingsTest, recordingsTrain, numTest, numTrain
+
+def recordingNameMapping(recordings, dictFem, dictMale):
+    nacta_out = []
+    for r in recordings:
+        if r in dictFem.keys():
+            nacta_out.append(['danAll', dictFem[r]])
+        if r in dictMale.keys():
+            nacta_out.append(['laosheng', dictMale[r]])
+
+    return nacta_out
+
+def getTestTrainRecordingArtist():
+
+    """
+    lineOnsetQmMale01 = getBoundaryNumber(queenMaryMale_01_Recordings, queenMarydataset_path)
+    lineOnsetQmMale02 = getBoundaryNumber(queenMaryMale_02_Recordings, queenMarydataset_path)
+    lineOnsetQmMale12 = getBoundaryNumber(queenMaryMale_12_Recordings, queenMarydataset_path)
+    lineOnsetQmMale13 = getBoundaryNumber(queenMaryMale_13_Recordings, queenMarydataset_path)
+
+    lineOnsetQmFem01 = getBoundaryNumber(queenMaryFem_01_Recordings, queenMarydataset_path)
+    lineOnsetQmFem10 = getBoundaryNumber(queenMaryFem_10_Recordings, queenMarydataset_path)
+    lineOnsetQmFem11 = getBoundaryNumber(queenMaryFem_11_Recordings, queenMarydataset_path)
+
+    lineOnsetLonDan = getBoundaryNumber(londonDan_Recordings, londonRecording_path)
+    lineOnsetLonLaosheng = getBoundaryNumber(londonLaosheng_Recordings, londonRecording_path)
+
+    lineOnsetBcnDan = getBoundaryNumber(bcnDan_Recordings, bcnRecording_path)
+    lineOnsetBcnLaosheng = getBoundaryNumber(bcnLaosheng_Recordings, bcnRecording_path)
+
+    numOnsetQmMale01 = [n[1] for n in lineOnsetQmMale01]
+    numOnsetQmMale02 = [n[1] for n in lineOnsetQmMale02]
+    numOnsetQmMale12 = [n[1] for n in lineOnsetQmMale12]
+    numOnsetQmMale13 = [n[1] for n in lineOnsetQmMale13]
+
+    numOnsetQmFem01 = [n[1] for n in lineOnsetQmFem01]
+    numOnsetQmFem10 = [n[1] for n in lineOnsetQmFem10]
+    numOnsetQmFem11 = [n[1] for n in lineOnsetQmFem11]
+
+    numOnsetLonDan = [n[1] for n in lineOnsetLonDan]
+    numOnsetLonLaosheng = [n[1] for n in lineOnsetLonLaosheng]
+
+    numOnsetBcnDan = [n[1] for n in lineOnsetBcnDan]
+    numOnsetBcnLaosheng = [n[1] for n in lineOnsetBcnLaosheng]
+
+    idx_test_QmMale01 = testRecordings(numOnsetQmMale01, 0.2)
+    idx_test_QmMale13 = testRecordings(numOnsetQmMale13, 0.2)
+
+    idx_test_QmFem01 = testRecordings(numOnsetQmFem01, 0.2)
+    idx_test_QmFem10 = testRecordings(numOnsetQmFem10, 0.2)
+
+    idx_test_LonDan = testRecordings(numOnsetLonDan, 0.2)
+    idx_test_LonLaosheng = testRecordings(numOnsetLonLaosheng, 0.2)
+
+    idx_test_BcnDan = testRecordings(numOnsetBcnDan, 0.2)
+    idx_test_BcnLaosheng = testRecordings(numOnsetBcnLaosheng, 0.2)
+
+    print(numOnsetQmMale02)
+    print(numOnsetQmMale12)
+    print(numOnsetQmFem11)
+
+    testQmFem01, trainQmFem01, numTestQmFem01, numTrainQmFem01 = recordingsNumTestTrainHelper(lineOnsetQmFem01, idx_test_QmFem01)
+    testQmFem10, trainQmFem10, numTestQmFem10, numTrainQmFem10 = recordingsNumTestTrainHelper(lineOnsetQmFem10, idx_test_QmFem10)
+
+    testQmMale01, trainQmMale01, numTestQmMale01, numTrainQmMale01 = recordingsNumTestTrainHelper(lineOnsetQmMale01, idx_test_QmMale01)
+    testQmMale13, trainQmMale13, numTestQmMale13, numTrainQmMale13 = recordingsNumTestTrainHelper(lineOnsetQmMale13, idx_test_QmMale13)
+
+    testBcnDan, trainBcnDan, numTestBcnDan, numTrainBcnDan = recordingsNumTestTrainHelper(lineOnsetBcnDan, idx_test_BcnDan)
+    testBcnLaosheng, trainBcnLaosheng, numTestBcnLaosheng, numTrainBcnLaosheng = recordingsNumTestTrainHelper(lineOnsetBcnLaosheng, idx_test_BcnLaosheng)
+
+    testLonDan, trainLonDan, numTestLonDan, numTrainLonDan = recordingsNumTestTrainHelper(lineOnsetLonDan, idx_test_LonDan)
+    testLonLaosheng, trainLonLaosheng, numTestLonLaosheng, numTrainLonLaosheng = recordingsNumTestTrainHelper(lineOnsetLonLaosheng, idx_test_LonLaosheng)
+
+    testNacta = testQmFem01+testQmFem10+testQmMale01+testQmMale13+testLonDan+testLonLaosheng+testBcnDan+testBcnLaosheng
+    trainNacta = trainQmFem01+trainQmFem10+trainQmMale01+trainQmMale13+trainLonDan+trainLonLaosheng+trainBcnDan+trainBcnLaosheng
+
+    # numTestNacta = sum(numTestQmFem01+numTestQmFem10+numTestQmMale01+numTestQmMale13+numTestLonDan+numTestLonLaosheng+numTestBcnDan+numTestBcnLaosheng)
+    # numTrainNacta = sum(numTrainQmFem01+numTrainQmFem10+numTrainQmMale01+numTrainQmMale13+numTrainLonDan+numTrainLonLaosheng+numTrainBcnDan+numTrainBcnLaosheng)
+
+    # print(testNacta+queenMaryMale_02_Recordings+queenMaryFem_11_Recordings, numTestNacta+numOnsetQmMale02[0]+numOnsetQmFem11[0])
+    # print(trainNacta+queenMaryMale_12_Recordings, numTrainNacta+numOnsetQmMale12[0])
+
+    testNacta = testNacta + queenMaryMale_02_Recordings + queenMaryFem_11_Recordings
+    trainNacta = trainNacta + queenMaryMale_12_Recordings
+
+    dictFem = dict(dict_name_mapping_dan_qm)
+    dictFem.update(dict_name_mapping_dan_bcn)
+    dictFem.update(dict_name_mapping_dan_london)
+
+    dictMale = dict(dict_name_mapping_laosheng_bcn)
+    dictMale.update(dict_name_mapping_laosheng_qm)
+    dictMale.update(dict_name_mapping_dan_london)
+
+    testNacta_out = recordingNameMapping(testNacta, dictFem, dictMale)
+    trainNacta_out = recordingNameMapping(trainNacta, dictFem, dictMale)
+
+    # print(testNacta_out)
+    # print(trainNacta_out)
+    """
+
+    testNacta_out = [['danAll', 'daxp-Zhe_cai_shi-Suo_lin_nang01-qm'], ['danAll', 'dafeh-Bi_yun_tian-Xi_xiang_ji01-qm'],
+     ['danAll', 'daspd-Hai_dao_bing-Gui_fei_zui_jiu02-qm'], ['laosheng', 'lseh-Wei_guo_jia-Hong_yang_dong02-qm'],
+     ['laosheng', 'lseh-Wo_ben_shi-Qiong_lin_yan-qm'], ['laosheng', 'lseh-Yi_lun_ming-Wen_zhao_guan-qm'],
+     ['laosheng', 'lsxp-Huai_nan_wang-Huai_he_ying02-qm'], ['danAll', 'dagbz-Feng_xiao_xiao-Yang_men_nv_jiang-lon'],
+     ['laosheng', 'dagbz-Feng_xiao_xiao-Yang_men_nv_jiang-lon'], ['danAll', 'daspd-Du_shou_kong-Wang_jiang_ting-upf'],
+     ['laosheng', 'lsxp-Wo_zheng_zai-Kong_cheng_ji01-upf'], ['laosheng', 'lsxp-Guo_liao_yi-Wen_zhao_guan02-qm'],
+     ['danAll', 'daxp-Jiao_Zhang_sheng-Hong_niang01-qm']]
+    trainNacta_out = [['danAll', 'danbz-Kan_dai_wang-Ba_wang_bie_ji01-qm'], ['danAll', 'daxp-Meng_ting_de-Mu_Gui_ying_gua_shuai04-qm'],
+     ['danAll', 'daxp-Jiao_Zhang_sheng-Hong_niang04-qm'], ['danAll', 'daxp-Chun_qiu_ting-Suo_lin_nang01-qm'],
+     ['danAll', 'danbz-Bei_jiu_chan-Chun_gui_men01-qm'], ['danAll', 'daxp-Meng_ting_de-Mu_Gui_ying_gua_shuai02-qm'],
+     ['laosheng', 'lseh-Tan_Yang_jia-Hong_yang_dong-qm'], ['laosheng', 'lseh-Zi_na_ri-Hong_yang_dong-qm'],
+     ['laosheng', 'lsxp-Xi_ri_you-Zhu_lian_zhai-qm'], ['laosheng', 'lsxp-Quan_qian_sui-Gan_lu_si-qm'],
+     ['laosheng', 'lsxp-Shi_ye_shuo-Ding_jun_shan-qm'], ['laosheng', 'lsxp-Wo_ben_shi-Kong_cheng_ji-qm'],
+     ['laosheng', 'lsxp-Wo_zheng_zai-Kong_cheng_ji04-qm'], ['laosheng', 'lsxp-Qian_bai_wan-Si_lang_tang_mu01-qm'],
+     ['danAll', 'daxp-Guan_Shi_yin-Tian_nv_san_hua-lon'], ['laosheng', 'daxp-Guan_Shi_yin-Tian_nv_san_hua-lon'],
+     ['danAll', 'daeh-Yang_Yu_huan-Tai_zhen_wai_zhuan-lon'], ['laosheng', 'daeh-Yang_Yu_huan-Tai_zhen_wai_zhuan-lon'],
+     ['danAll', 'daspd-Hai_dao_bing-Gui_fei_zui_jiu01-lon'], ['laosheng', 'daspd-Hai_dao_bing-Gui_fei_zui_jiu01-lon'],
+     ['danAll', 'daxp-Meng_ting_de-Mu_Gui_ying_gua_shuai01-upf'],
+     ['laosheng', 'lsxp-Jiang_shen_er-San_jia_dian01-1-upf'], ['laosheng', 'lsxp-Jiang_shen_er-San_jia_dian01-2-upf'],
+     ['laosheng', 'lsxp-Guo_liao_yi-Wen_zhao_guan01-upf'], ['laosheng', 'lsxp-Jiang_shen_er-San_jia_dian02-qm']]
+
+    return testNacta_out, trainNacta_out
+
 def getRecordingNames(stringTrainTest='TRAIN'):
     if stringTrainTest == 'TRAIN':
         return [['male_01/neg_3', 'male_01/neg_4', 'male_01/neg_5', 'male_01/pos_1',
@@ -169,4 +302,5 @@ def getRecordingNames(stringTrainTest='TRAIN'):
 
 if __name__ == '__main__':
     # getTestTrainRecordings()
-    print getRecordingNames('TRAIN')
+    # print getRecordingNames('TRAIN')
+    getTestTrainRecordingArtist()
